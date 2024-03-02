b355e00a78c2ab8af354b34d5eea23e3d20de500
sanjana <sanjanatiwari025@gmail.com>
2024-03-02 11:18:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import './App.css'
+  // App.js
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function CounterApp() {
+  const [inputValue, setInputValue] = useState('');
+  const [counter, setCounter] = useState(0);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAdd = (value) => {
+    setCounter((prevCounter) => prevCounter + value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter-app">
+      <input
+        type="number"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter a number"
+      />
+      <div className="buttons">
+        <button onClick={() => handleAdd(1)}>+1</button>
+        <button onClick={() => handleAdd(-1)}>-1</button>
+        <button onClick={() => handleAdd(10)}>+10</button>
+        <button onClick={() => handleAdd(-10)}>-10</button>
+      </div>
+      <p>Counter: {counter}</p>
     </div>
   );
 }
 
-export default App;
+export default CounterApp;
+
